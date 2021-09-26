667dadaa90c52f1fd33d0a5295e33874a47c5123
cdgus1514 <37209022+cdgus1514@users.noreply.github.com>
2021-09-26 11:52:10 +0900

    프로젝트 생성
    엔티티 설계 및 매핑

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    private int orderprice;
+
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getOrderprice() {
+        return orderprice;
+    }
+
+    public void setOrderprice(int orderprice) {
+        this.orderprice = orderprice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
